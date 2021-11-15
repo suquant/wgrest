3508d2063f373ff1b85743513fee741b58eec3d9
George Kutsurua <g.kutsurua@gmail.com>
2021-11-15 23:21:00 +0400

    clean
    Signed-off-by: George Kutsurua <g.kutsurua@gmail.com>

--- clients/typeascript-axios/models/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './device';
-export * from './device-create-or-update-request';
-export * from './model-error';
-export * from './peer';
-export * from './peer-create-or-update-request';
